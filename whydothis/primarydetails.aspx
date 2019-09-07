05518e5ba402457839f5bbbc4f655ed60b98357e
Amitoj Singh <amitojsingh@hotmail.com>
2019-09-07 17:42:16 +0530

    Add project files.

---
+++ whydothis/primarydetails.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="primarydetails.aspx.cs" Inherits="whydothis.primarydetails" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="LabelFirstName" runat="server" Text="FirstName "></asp:Label>     
+            <asp:TextBox ID="TextFirstName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelLastName" runat="server" Text="LastName "></asp:Label>
+            <asp:TextBox ID="TextLastName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelAge" runat="server" Text="Age "></asp:Label>
+            <asp:TextBox ID="TextAge" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelGender" runat="server" Text="Gender "></asp:Label>
+            <asp:RadioButton ID="RadioMale" runat="server" Text="Male" Checked="true" GroupName="Gender" />
+            <asp:RadioButton ID="RadioFemale" Text="Female" runat="server" GroupName="Gender" />
+            <br />
+            <asp:Label ID="LabelPAN" runat="server" Text="PAN "></asp:Label>
+            <asp:TextBox ID="TextPAN" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelLicense" runat="server" Text="License "></asp:Label>
+            <asp:TextBox ID="TextLicense" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelPhone" runat="server" Text="Phone "></asp:Label>
+            <asp:TextBox ID="TextPhone" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="LabelHobbies" runat="server" Text="Select Your Hobbies"></asp:Label>
+            <asp:CheckBoxList ID="CheckHobbies" runat="server">
+                
+            </asp:CheckBoxList>
+            <br />
+            <asp:Button ID="Save" runat="server" Text="Save" OnClick="Save_Click" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ whydothis/primarydetails.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace whydothis
+{
+    public partial class primarydetails : System.Web.UI.Page
+    {
+        SqlConnection connection = new SqlConnection();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            connection.ConnectionString = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            if (!IsPostBack)
+            {
+                BindInterests();
+            }
+        }
+
+        protected void BindInterests()
+        {
+            SqlCommand sql = new SqlCommand("SELECT * FROM interests", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(sql);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            CheckHobbies.DataSource = ds.Tables[0];
+            CheckHobbies.DataTextField = "interest_name";
+            CheckHobbies.DataValueField = "interest_id";
+            CheckHobbies.DataBind();
+        }
+
+        protected void Save_Click(object sender, EventArgs e)
+        {
+            AddPrimaryInfo();
+        }
+
+        public Int32 AddPrimaryInfo()
+        {
+            SqlCommand sql = new SqlCommand("AddPrimary", connection);
+            sql.CommandType = CommandType.StoredProcedure;
+            sql.Parameters.AddWithValue("@fname", TextFirstName.Text);
+            sql.Parameters.AddWithValue("@lname", TextLastName.Text);
+            sql.Parameters.AddWithValue("@age", TextAge.Text);
+            if (RadioMale.Checked == true)
+            {
+                sql.Parameters.AddWithValue("@gender", "male");
+            }
+            else
+            {
+                sql.Parameters.AddWithValue("@gender", "female");
+            }
+            sql.Parameters.AddWithValue("@pan", TextPAN.Text);
+            sql.Parameters.AddWithValue("@license", TextLicense.Text);
+            sql.Parameters.AddWithValue("@phone", TextPhone.Text);
+            sql.Parameters.Add("@temp_emp", SqlDbType.Int);
+            sql.Parameters["@temp_emp"].Direction = ParameterDirection.Output;
+            sql.ExecuteNonQuery();
+            Int32 k = Convert.ToInt32(sql.Parameters["@temp_emp"].Value);
+            sql.Dispose();
+           
+            foreach (ListItem item in CheckHobbies.Items)
+            {
+                if (item.Selected == true)
+                {
+                    SqlCommand command = new SqlCommand("AddInterests", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("emp_id", k);
+                    command.Parameters.AddWithValue("@interest_id", item.Value);
+                    command.ExecuteNonQuery();
+                    command.Dispose();
+                }
+            }
+            
+            Response.Write("<script>alert('Success!')</script>");
+
+            return k;
+        }        
+    }
+}
